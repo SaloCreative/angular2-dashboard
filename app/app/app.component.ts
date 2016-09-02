388ad8d0c6a7e79fff33141193de665d3af72209
richcomber <rich@freshleafmedia.co.uk>
2016-09-02 14:24:09 +0100

    Adding basic front end loveliness

--- app/app/app.component.ts
+++ app/app/app.component.ts
@@ -1,13 +1,7 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'app',
-    template: `
-    <h1>{{title}}</h1>
-    <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-    <a routerLink="/clients" routerLinkActive="active">Clients</a>
-    <a routerLink="/projects" routerLinkActive="active">Projects</a>
-    <router-outlet></router-outlet>
-  `
+    templateUrl: 'views/master.html'
 })
 export class AppComponent {
     title = 'Dummy Intranet';
