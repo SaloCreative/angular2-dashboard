56e84ce2236db998a1faa6bf62a6a980a81abad9
Rich Comber <rich@elementalwebdesign.co.uk>
2016-09-03 09:44:18 +0100

    Changing base project over to dashboard

--- app/app/app.endpoints.ts
+++ app/app/app.endpoints.ts
@@ -1,6 +1,6 @@
 export class Api {
     public static getEndPoint(value: string) {
-        let base = 'http://192.168.1.150/api.intranet2.freshleafmedia.co.uk/public/api/v1/';
+        let base = 'local.api.intranet2.freshleafmedia.co.uk/api/v1/';
         var data = {
             'projects': 'projects',
             'clients': 'clients'
